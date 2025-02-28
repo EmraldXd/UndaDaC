14661f72dc3ec5f4ef135092dc1872b0c3ceb991
EmraldXd <zekecrutch@gmail.com>
2025-02-28 14:56:12 -0500

    It work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/DistanceSensorTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -11,6 +12,7 @@ import org.firstinspires.ftc.teamcode.action.mecanumDrive;
  * This OpMode Exists so that we can test our Rev 2M Distance sensor. This allows us to know whether
  * or not we as programmers understand how the sensor works or not.
  */
+@TeleOp
 public class DistanceSensorTest extends OpMode {
     mecanumDrive mecanumDrive = new mecanumDrive();
     DistanceSensor distSense;
